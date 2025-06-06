cdf0227a186df618bea8e24c0cf850650d04de47
Tamagarden <deathlyyogurt@gmail.com>
2025-06-06 09:05:35 +0700

    Add files via upload

---
+++ yuki-chatbot/public/yuki-chat/client.js
@@ -0,0 +1,13 @@
+
+async function sendMessage() {
+  const input = document.getElementById("userInput").value;
+  const res = await fetch("/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ message: input })
+  });
+  const data = await res.json();
+  document.getElementById("chatLog").innerHTML += '<p><b>ยูกิ:</b> ' + data.reply + '</p>';
+}
